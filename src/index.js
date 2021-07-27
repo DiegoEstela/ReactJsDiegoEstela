0319ad219dbfc65ec59df0d6b525aa93323f2baa
Diego Ezequiel Estela <83254494+Diezte@users.noreply.github.com>
2021-07-27 02:07:15 -0300

    Se instalo Bootstrap, se definio NavBar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { NabVar } from './components/NabVar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <NabVar />,
