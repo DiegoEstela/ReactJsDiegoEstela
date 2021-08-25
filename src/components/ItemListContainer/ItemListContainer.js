5e4b85ba17c9f11cd8dc22eeeef0a0b218d4a77f
Diego Ezequiel Estela <die.estela@gmail.com>
2021-08-25 18:43:26 -0300

    Revert "Se Agrego Spinner, aguardando los productos"
    This reverts commit a8230979664376e7ae0cd8597a0ec00211bf88ff.

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -5,19 +5,17 @@ import productos from "../Apis/ItemList";
 
 export function ItemListContainer() {
   const [Prod, setProd] = useState([]);
-  const [Load, setLoad] = useState(true);
 
   useEffect(() => {
     const task = () =>
       new Promise((resolve, reject) => {
         setTimeout(() => {
           resolve(productos);
-          setLoad(false);
-        }, 3000);
+        }, 2000);
       });
 
     task().then((data) => setProd(data));
   }, []);
 
-  return <ItemList items={Prod} Load={Load} />;
+  return <ItemList items={Prod} />;
 }
