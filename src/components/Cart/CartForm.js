af355d492609d5f6cbc6070b6fd6afb4369e315f
Diego Ezequiel Estela <die.estela@gmail.com>
2021-09-02 01:07:15 -0300

    Se Configuro el Formulario para que envie data a Firebase

--- src/components/Cart/CartForm.js
+++ src/components/Cart/CartForm.js
@@ -5,19 +5,17 @@ import { CartContext } from "../../Context/CartContext";
 
 export const CartForm = () => {
   const { cart, calcularTotal } = useContext(CartContext);
-  const [orderCreatedId, setOrderCreatedId] = useState(null);
+  const [OrdenId, setOrdenId] = useState(null);
   const [UserInfo, setUserInfo] = useState({
     nombre: "",
     telefono: "",
     email: "",
   });
-  console.log(UserInfo);
-  console.log(orderCreatedId);
+  console.log(OrdenId);
+  const { nombre, telefono, email } = UserInfo;
   const handleChangeUser = (event) => {
     setUserInfo({ ...UserInfo, [event.target.name]: event.target.value });
   };
-
-  const { nombre } = UserInfo;
 
   const finalizarCompra = (event) => {
     event.preventDefault();
@@ -32,9 +30,9 @@ export const CartForm = () => {
 
     const nuevaOrden = {
       buyer: {
-        name: nombre,
-        email: "que",
-        telefono: "nada",
+        Nombre: nombre,
+        Email: email,
+        Telefono: telefono,
       },
       items: newItems,
       total: calcularTotal(),
@@ -50,66 +48,82 @@ export const CartForm = () => {
         batch.update(docRef, { stock: item.stock - item.stock });
       });
       batch.commit();
-      setOrderCreatedId(response.id);
+      setOrdenId(response.id);
     });
   };
 
   return (
-    <Form className="form" onSubmit={finalizarCompra}>
-      <Row className="mb-3">
-        <Form.Group as={Col} md="4" controlId="validationCustom01">
-          <Form.Label>Nombre</Form.Label>
-          <Form.Control type="text" placeholder="Nombre" name="nombre" />
-          <Form.Control.Feedback>Muy Bien!</Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="4">
-          <Form.Label>Telefono</Form.Label>
-          <Form.Control type="text" placeholder="telefono" name="telefono" />
-          <Form.Control.Feedback>Muy Bien!</Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-          <Form.Label>Email</Form.Label>
-          <InputGroup hasValidation>
-            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+    <div>
+      <Form className="form" onSubmit={finalizarCompra}>
+        <Row className="mb-3">
+          <Form.Group as={Col} md="4" controlId="validationCustom01">
+            <Form.Label>Nombre Y Apellido</Form.Label>
             <Form.Control
               type="text"
-              placeholder="Email"
-              aria-describedby="inputGroupPrepend"
-              required
+              placeholder="Nombre Y Apellido"
+              name="nombre"
+              value={nombre}
+              onChange={handleChangeUser}
             />
+            <Form.Control.Feedback>Muy Bien!</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="4">
+            <Form.Label>Telefono</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Telefono"
+              name="telefono"
+              value={telefono}
+              onChange={handleChangeUser}
+            />
+            <Form.Control.Feedback>Muy Bien!</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+            <Form.Label>Email</Form.Label>
+            <InputGroup hasValidation>
+              <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+              <Form.Control
+                type="text"
+                placeholder="Email"
+                name="email"
+                aria-describedby="inputGroupPrepend"
+                value={email}
+                onChange={handleChangeUser}
+              />
+              <Form.Control.Feedback type="invalid">
+                Por favor coloca tu Email.
+              </Form.Control.Feedback>
+            </InputGroup>
+          </Form.Group>
+        </Row>
+        <Row className="mb-3">
+          <Form.Group as={Col} md="6" controlId="validationCustom03">
+            <Form.Label>Direccion</Form.Label>
+            <Form.Control type="text" placeholder="Direccion" required />
             <Form.Control.Feedback type="invalid">
-              Por favor coloca tu Email.
+              Por favor coloca tu direccion.
             </Form.Control.Feedback>
-          </InputGroup>
+          </Form.Group>
+          <Form.Group as={Col} md="3" controlId="validationCustom04">
+            <Form.Label>Localidad</Form.Label>
+            <Form.Control type="text" placeholder="Localidad" required />
+            <Form.Control.Feedback type="invalid">
+              Por favor coloca tu localidad.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="3" controlId="validationCustom05">
+            <Form.Label>Codigo Postal</Form.Label>
+            <Form.Control type="text" placeholder="CP" required />
+            <Form.Control.Feedback type="invalid">
+              Por favor coloca tu CP.
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <Form.Group className="mb-3">
+          <Form.Check label="¿Queres recibir nuestras novedades?" />
         </Form.Group>
-      </Row>
-      <Row className="mb-3">
-        <Form.Group as={Col} md="6" controlId="validationCustom03">
-          <Form.Label>Direccion</Form.Label>
-          <Form.Control type="text" placeholder="Direccion" required />
-          <Form.Control.Feedback type="invalid">
-            Por favor coloca tu direccion.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="3" controlId="validationCustom04">
-          <Form.Label>Localidad</Form.Label>
-          <Form.Control type="text" placeholder="Localidad" required />
-          <Form.Control.Feedback type="invalid">
-            Por favor coloca tu localidad.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="3" controlId="validationCustom05">
-          <Form.Label>Codigo Postal</Form.Label>
-          <Form.Control type="text" placeholder="CP" required />
-          <Form.Control.Feedback type="invalid">
-            Por favor coloca tu CP.
-          </Form.Control.Feedback>
-        </Form.Group>
-      </Row>
-      <Form.Group className="mb-3">
-        <Form.Check label="¿Queres recibir nuestras novedades?" />
-      </Form.Group>
-      <Button type="submit">Finalizar compra</Button>
-    </Form>
+        <Button type="submit">Finalizar compra</Button>
+      </Form>
+    </div>
   );
 };
